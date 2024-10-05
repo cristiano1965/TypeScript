553acd37b4065d5a48418c159fbdc6106ba9c79a
cristiano <cris6527.cp@gmail.com>
2024-10-05 12:18:04 +0200

    coding completo, corso finito

--- app.ts
+++ app.ts
@@ -1,4 +1,17 @@
-function somma(num1: number, num2 = 0) {
-  return num1 + num2;
+function creaElemento(elemento: string, id: string, nome: string) {
+  return function (constructor: any) {
+    const container = document.getElementById(id);
+    const persona = new constructor(nome);
+    if (container) {
+      container.innerHTML = elemento;
+      container.querySelector("h1")!.textContent = persona.nome;
+    }
+  };
 }
-console.log(somma(4, 7));
+
+@creaElemento("<h1></h1>", "container", "Luca")
+class Persona {
+  constructor(public nome: string) {
+    console.log("sto creando una persona denominata " + this.nome);
+  }
+}
